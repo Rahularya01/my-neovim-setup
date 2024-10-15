8a9188fc207f5217a88a30e162227050f453128d
Rahul Arya <aryarahul819@gmail.com>
2024-10-15 22:08:19 +0530

    neovim surround

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,17 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local telescope = require("telescope")
+local actions = require("telescope.actions")
+
+telescope.setup({
+  defaults = {
+    file_ignore_patterns = { "node_modules", ".git", ".next", "dist", "build" },
+    mappings = {
+      i = {
+        ["<esc>"] = actions.close, -- Close on escape in insert mode
+      },
+    },
+  },
+})
